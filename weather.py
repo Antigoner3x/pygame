c1e5e418afbc3abe6e8a7fd694f4d2c8bd6276bf
Antigoner3x <slauk@asu.edu>
2014-03-23 16:22:52 -0700

    parsing weather and dates
    it is __________F in Happy Town!
    March 23

--- weather.py
+++ weather.py
@@ -1,12 +1,15 @@
 import pywapi
 import string
+import pprint
 
-weather_com_result = pywapi.get_weather_from_weather_com('85296')
-yahoo_result = pywapi.get_weather_from_yahoo('85296')
-noaa_result = pywapi.get_weather_from_noaa('KJFK')
+pp = pprint.PrettyPrinter(indent=4)
+#weather_com_result = pywapi.get_weather_from_weather_com('85296')
+yahoo_result = pywapi.get_weather_from_yahoo('USAZ0082', 'imperial')
+#noaa_result = pywapi.get_weather_from_noaa('KJFK')
 
-print "Weather.com says: It is " + string.lower(weather_com_result['current_conditions']['text']) + " and " + weather_com_result['current_conditions']['temperature'] + "C now in New York.\n\n"
+#print "Weather.com says: It is " + string.lower(weather_com_result['current_conditions']['text']) + " and " + weather_com_result['current_conditions']['temperature'] + "C now in New York.\n\n"
 
-print "Yahoo says: It is " + string.lower(yahoo_result['condition']['text']) + " and " + yahoo_result['condition']['temp'] + "C now in New York.\n\n"
+print "Yahoo says: It is " + string.lower(yahoo_result['condition']['text']) + " and " + yahoo_result['condition']['temp'] + "F now in Arizona.\n\n" + yahoo_result['condition']['date']
 
-print "NOAA says: It is " + string.lower(noaa_result['weather']) + " and " + noaa_result['temp_c'] + "C now in New York.\n"
+pp.pprint(yahoo_result['forecasts'])
+#print "NOAA says: It is " + string.lower(noaa_result['weather']) + " and " + noaa_result['temp_c'] + "C now in New York.\n"
